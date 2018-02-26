ba2c4127935594b9cfe8bd10533c8e96a00c9c16
MKody <im@kdy.ch>
2018-02-26 21:16:17 +0100

    Oh, didn't remove the require()

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -7,7 +7,6 @@ var merge = require('webpack-merge')
 var baseWebpackConfig = require('./webpack.base.conf')
 var CopyWebpackPlugin = require('copy-webpack-plugin')
 var HtmlWebpackPlugin = require('html-webpack-plugin')
-var SitemapPlugin = require('sitemap-webpack-plugin').default
 var ExtractTextPlugin = require('extract-text-webpack-plugin')
 var OptimizeCSSPlugin = require('optimize-css-assets-webpack-plugin')
 var SWPrecacheWebpackPlugin = require('sw-precache-webpack-plugin')
